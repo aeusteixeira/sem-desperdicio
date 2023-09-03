7c37a5c6b08ce92e68ea58a16d533061005521b9
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2023-09-03 13:49:41 -0300

    Adicionado dominio de teste

--- info.php
+++ info.php
@@ -1,26 +1,12 @@
 <?php
-// Lista de domínios permitidos
-$allowedOrigins = [
-    'https://semdesperdicio.eco.br', // Adicione seu domínio aqui
-    'http://localhost',
-    'http://localhost/sem-desperdicio',
+$origin = $_SERVER['HTTP_ORIGIN'];
+$allowed_domains = [
+    'https://semdesperdicio.eco.br/',
 ];
 
-// Verifica se o cabeçalho "Origin" está presente na requisição
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    $origin = $_SERVER['HTTP_ORIGIN'];
-
-    // Verifica se a origem está na lista de domínios permitidos
-    if (in_array($origin, $allowedOrigins)) {
-        // Retorne "OK" se a origem estiver permitida
-        echo "OK";
-    } else {
-        // Retorne um erro se a origem não estiver na lista permitida
-        http_response_code(403); // Código de status "Proibido"
-        echo "Acesso negado! (HTTP_ORIGIN não permitido)";
-    }
-} else {
-    // Retorne "OK" se o cabeçalho "Origin" não estiver presente
-    echo "OK";
+if (in_array($origin, $allowed_domains)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+    echo 'Hello, world!';
+}else{
+    echo 'Not allowed';
 }
-?>
