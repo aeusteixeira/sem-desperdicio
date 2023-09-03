a5b2657fcac5eb3708bc495d0ac575c68d1a2f82
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2023-09-03 14:33:53 -0300

    Adicionado a proteção na API

--- request.php
+++ request.php
@@ -1,32 +1,47 @@
 <?php
 
+$origin = $_SERVER['HTTP_HOST'];
+$allowed_domains = [
+    'https://semdesperdicio.eco.br',
+    'https://www.semdesperdicio.eco.br',
+    'https://semdesperdicio.eco.br/',
+    'https://www.semdesperdicio.eco.br/',
+    'semdesperdicio.eco.br',
+];
+
 require __DIR__ . '/vendor/autoload.php';
 $config = require_once(__DIR__ . '/config/config.php');
 use Orhanerday\OpenAi\OpenAi;
 
-$ingredients = $_GET['ingredients'];
+if (in_array($origin, $allowed_domains)) {
 
-$open_ai = new OpenAi($config['OPEN_IA_KEY']);
+    $ingredients = $_GET['ingredients'];
 
-$prompt = "Você é um ex-chef de cozinha que trabalha em um blog que ajuda as pessoas a criarem pratos com os ingredients que tem em casa. Preciso que você me de o passo a passo de como construir uma refeição, prato de comida ou lanche com o que as pessoas tem em casa. O que o cliente te passou foi:" . $ingredients;
+    $open_ai = new OpenAi($config['OPEN_IA_KEY']);
 
-$response = $open_ai->completion([
-    'model' => 'text-davinci-002',
-    'prompt' => $prompt,
-    'temperature' => 0.9,
-    'max_tokens' => 1200,
-    'frequency_penalty' => 0,
-    'presence_penalty' => 0.6,
-]);
+    $prompt = "Você é um ex-chef de cozinha que trabalha em um blog que ajuda as pessoas a criarem pratos com os ingredients que tem em casa. Preciso que você me de o passo a passo de como construir uma refeição, prato de comida ou lanche com o que as pessoas tem em casa. O que o cliente te passou foi:" . $ingredients;
 
-$jsonResponse = json_decode($response, true);
-$recipeText = $jsonResponse['choices'][0]['text'];
+    $response = $open_ai->completion([
+        'model' => 'text-davinci-002',
+        'prompt' => $prompt,
+        'temperature' => 0.9,
+        'max_tokens' => 1200,
+        'frequency_penalty' => 0,
+        'presence_penalty' => 0.6,
+    ]);
 
-// Define o cabeçalho Content-Type para indicar que o conteúdo é JSON
-header('Content-Type: application/json');
+    $jsonResponse = json_decode($response, true);
+    $recipeText = $jsonResponse['choices'][0]['text'];
 
-// Retorna a resposta JSON para o frontend
-echo json_encode([
-    'recipe' => $recipeText
-]);
+    // Define o cabeçalho Content-Type para indicar que o conteúdo é JSON
+    header('Content-Type: application/json');
 
+    // Retorna a resposta JSON para o frontend
+    echo json_encode([
+        'recipe' => $recipeText
+    ]);
+
+} else {
+    http_response_code(403);
+    echo 'Acesso negado! (Host não permitido)';
+}
