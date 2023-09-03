21e85dc30b70920793ec934c161e296cd03725e1
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2023-09-03 13:52:48 -0300

    Adicionado dominio de teste

--- info.php
+++ info.php
@@ -1,14 +1,17 @@
 <?php
-$origin = $_SERVER['HTTP_ORIGIN'];
-$allowed_domains = [
-    'https://semdesperdicio.eco.br/',
-];
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    $origin = $_SERVER['HTTP_ORIGIN'];
+    $allowed_domains = [
+        'https://semdesperdicio.eco.br/',
+    ];
 
-var_dump(in_array($origin, $allowed_domains));
-
-if (in_array($origin, $allowed_domains)) {
-    header('Access-Control-Allow-Origin: ' . $origin);
-    echo 'Hello, world!';
-}else{
-    echo 'Not allowed';
+    if (in_array($origin, $allowed_domains)) {
+        header('Access-Control-Allow-Origin: ' . $origin);
+        echo 'API is working';
+    } else {
+        echo 'API is not allowed'
+    }
+} else {
+    echo 'No origin'
 }
+?>
